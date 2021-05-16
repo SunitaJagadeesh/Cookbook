9fe5fdd238851ddb51e31f9a33fb7af66deadb6f
sunita <sunitavangapandu@gmail.com>
2021-05-16 12:17:17 +0530

    changes in comments.dart

--- lib/view/comments.dart
+++ lib/view/comments.dart
@@ -1,3 +1,4 @@
+import 'package:cookbook_app/utils/rounded_button.dart';
 import 'package:cookbook_app/utils/rounded_input_field.dart';
 import 'package:flutter/material.dart';
 
@@ -8,10 +9,15 @@ class Comments extends StatefulWidget {
 
 class _CommentsState extends State<Comments> {
   List<String> _comments = [];
-
+  String _commentText = "";
   void _addComments(String val){
     setState(() {
       _comments.add(val);
+    });
+  }
+  void _setComment(String val){
+    setState(() {
+      _commentText = val;
     });
   }
 
@@ -25,21 +31,48 @@ class _CommentsState extends State<Comments> {
         });
   }
   Widget _buildCommentItem(String comment) {
-    return ListTile(title: Text('Comments Page'),);
+    return ListTile(title: Text(comment),);
   }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Column(
         children: <Widget>[
         Expanded(child: _buildCommentList()),
-        RoundedInputField(
-          onChanged: (String submittedStr){
-            _addComments(submittedStr);
-          },
-          hintText: 'Add Comments',
-          obscure: false,
+        Row(
+          children: [
+            RoundedInputField(
+              hintText: 'Add Comments',
+              obscure: false,
+              onChanged:(String value) async { _setComment(value); },
+            ) ,
+            /*
+            RoundedButton(
+              text: 'Enter',
+              press: (String submittedStr){
+                _addComments(submittedStr);
+              },
+            )
+             */
+            Container(
+             // padding: EdgeInsets.symmetric(vertical: 50),
+              height: size.width * 0.1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(29),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF1B5E20)
+                  ),
+                    onPressed: (){
+                    print('Button pressed.');
+                    _addComments(_commentText);
+                    },
+                    child: Text('Send')),
+              ),
+            )
+          ],
         ),
       ],)
     );
